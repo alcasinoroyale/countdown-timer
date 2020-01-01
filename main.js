2eef9d5dbae97bc2298e1474c962eacb08a2e9d0
alcasinoroyale <alexzdatny@gmail.com>
2020-01-01 16:32:06 -0500

    Finish CSS, adjust some elements from tutorial

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ loadEventListeners();
 function loadEventListeners() {
 	document.addEventListener('DOMContentLoaded', function() { calcTime(); });
 };
+
+let timeTo = document.getElementById('time-to').value,
+		date,
+		now = new Date(),
+		newYear = new Date('1.1.2020').getTime(),
+		startTimer = '';
