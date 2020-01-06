f462da70d3450fcc70ebcb9f8a29fcf837b0d4c3
alcasinoroyale <alexzdatny@gmail.com>
2020-01-06 02:19:51 -0500

    Update the JS functions and styling for countdown timer

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Countdown Timer Functions //
+// load event listeners
 loadEventListeners();
 
 function loadEventListeners() {
@@ -10,3 +10,41 @@ let timeTo = document.getElementById('time-to').value,
 		now = new Date(),
 		newYear = new Date('1.1.2020').getTime(),
 		startTimer = '';
+
+function calcTime(dates) {
+	clearInterval(startTimer);
+
+	if (typeof(dates) == 'undefined'){
+		date = new Date(newYear).getTime();
+	} else {
+		date = new Date(dates).getTime();
+	}
+
+	function updateTimer(date){
+
+		let now = new Date().getTime();
+		let distance = date - now;
+
+		// Time calculations for days, hours, minutes and seconds
+		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		// select element
+		document.querySelector('.clock-days').innerHTML = days;
+		document.querySelector('.clock-hours').innerHTML = hours;
+		document.querySelector('.clock-minutes').innerHTML = minutes;
+		document.querySelector('.clock-seconds').innerHTML = seconds;
+
+		if(now >= date){
+			clearInterval(startTimer);
+			document.querySelector('.clock-days').innerHTML = 'D';
+			document.querySelector('.clock-hours').innerHTML = 'O';
+			document.querySelector('.clock-minutes').innerHTML = 'N';
+			document.querySelector('.clock-seconds').innerHTML = 'E';
+		}
+	}
+
+	startTimer = setInterval(function() { updateTimer(date); }, 1000);
+}
